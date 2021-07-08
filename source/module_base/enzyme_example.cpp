0da45f075bf7cb674b772c689df88ba8938bfcce
tansongchen <songchen@mit.edu>
2021-07-08 15:54:34 +0800

    Differentiate C/C++ stdlib

--- source/module_base/enzyme_example.cpp
+++ source/module_base/enzyme_example.cpp
@@ -1,24 +1,51 @@
 #include "matrix.h"
+#include "math.h"
+#include <vector>
+#include <numeric>
 #include <iostream>
-#define grad __enzyme_autodiff
+#define D __enzyme_autodiff
 
-void grad(double (*)(matrix *), matrix *, matrix *);
+using namespace std;
+
+int enzyme_const;
+int enzyme_out;
+int enzyme_dup;
+
+template<typename Return, typename ...T>
+Return D(T...);
+
+double wrapper_norm(matrix *m){ return m->norm(); }
+double wrapper_max(matrix *m){ return m->max(); }
+double wrapper_trace(matrix *m){ return m->trace_on(); }
+double calc(double d){ return exp(d * d) / 2.0; };
+double scale_sum(const vector<double> &vd, double scale) {
+    return scale * accumulate(vd.begin(), vd.end(), 0.0);
+}
 
 int main() {
+    cout << "Differentiating C standard library..." << endl;
+    cout << D<double>(calc, 1.0) << endl << endl;
+    cout << "Differentiating C++ standard library..." << endl;
+    vector<double> vd(10, 1);
+    double scale = 3;
+    cout << D<double>(scale_sum,
+        enzyme_const, vd,
+        enzyme_out, scale
+    ) << endl << endl;
     matrix m(2, 2);
     m(0, 0) = 1; m(0, 1) = 2; m(1, 0) = 3; m(1, 1) = 4;
     matrix _m(2, 2);
-    std::cout << "Differentiating Fröbenius norm..." << std::endl;
+    cout << "Differentiating Fröbenius norm..." << endl;
     _m.zero_out();
-    grad([](matrix *m){ return m->norm(); }, &m, &_m);
-    std::cout << _m << std::endl;
-    std::cout << "Differentiating max..." << std::endl;
+    D<double>(wrapper_norm, &m, &_m);
+    cout << _m << endl;
+    cout << "Differentiating max..." << endl;
     _m.zero_out();
-    grad([](matrix *m){ return m->max(); }, &m, &_m);
-    std::cout << _m << std::endl;
-    std::cout << "Differentiating trace..." << std::endl;
+    D<double>(wrapper_max, &m, &_m);
+    cout << _m << endl;
+    cout << "Differentiating trace..." << endl;
     _m.zero_out();
-    grad([](matrix *m){ return m->trace_on(); }, &m, &_m);
-    std::cout << _m << std::endl;
+    D<double>(wrapper_trace, &m, &_m);
+    cout << _m << endl;
     return 0;
 }
